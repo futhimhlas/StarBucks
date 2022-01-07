357018336fe037463f003a7ea0304d95ee059bdb
futhimhlas <futhimhl2@gmail.com>
2022-01-07 18:47:38 +0000

    Mobile menu added. HTML CSS and JS all added for mobile menu

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const btn = document.getElementById('menu-btn') /* Creating constant and assigning it by id */
+const nav = document.getElementById('menu')
 
 function navToggle() {
     btn.classList.toggle('open')
+    nav.classList.toggle('hidden')
+    document.body.classList.toggle('no-scroll')
 }
+
 btn.addEventListener('click', navToggle) /* Event listener added to our button and whenever its clicked we call a function called NavToggle */
